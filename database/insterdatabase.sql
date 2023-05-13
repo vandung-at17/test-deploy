428c5d9d981bb8f92fcd09978d1da28975414f7a
vandung-at17 <dungsongoku3@gmail.com>
2023-05-13 13:12:57 +0700

    UPDATE NEW NHAT

--- database/insterdatabase.sql
+++ database/insterdatabase.sql
@@ -1,1 +1,3 @@
-INSERT INTO `categories` VALUES (4,NULL,'Rau',1),(5,NULL,'Trái cây',1),(6,NULL,'Củ quả',1);
+INSERT INTO `railway`.`categories` (`category_id`, `category_name`, `status`) VALUES ('4', 'Rau', true);
+INSERT INTO `railway`.`categories` (`category_id`, `category_name`, `status`) VALUES ('5', 'Trái Cây', true);
+INSERT INTO `railway`.`categories` (`category_id`, `category_name`, `status`) VALUES ('6', 'Củ Quả', true);
